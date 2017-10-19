5278944b914af2d96fccf6ce3bc5a2b40064def1
Spencer <yeshaopeng@lianqin360.com>
2017-10-19 11:02:23 +0800

    1.1.2

--- dest/pipejs.js
+++ dest/pipejs.js
@@ -1,22 +1,32 @@
 (function (factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() : typeof define === 'function' && define.amd ? define(factory) : window.Pipe = factory();     
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() : typeof define === 'function' && define.amd ? define(factory) : window.Pipe = factory();
 })(function () {
     'use strict'
 
     var IDLE = 'idle';
     var BUSY = 'busy';
+    var id = 0;
+    var KEY = '_pipe_x_key_';
 
-    function next(pipe, source) {
-        var handler = pipe._handler;
+    function key() {
+        return 'pipe_x_key_' + (++id);
+    }
+    function firstNoop(source, next, abort) {
+        next();
+    }
+    function lastNoop(source, next, abort) {
+        next();
+    }
+    function next(key,handler,source) {
         function _abort() {
 
         }
         function _next(newSource) {
             source = newSource || source;
-            if (pipe._next) {
-                next(pipe._next, source)
+            var nextHandler = getNext(key,handler);
+            if (nextHandler) {
+                next(key,nextHandler, source)
             } else {
-                pipe._root.status = IDLE;
             }
         }
         handler(source, _next, _abort);
@@ -24,74 +34,108 @@
     }
 
     function abort(pipe) {
-        pipe._root.status = IDLE;
+
     }
 
-    function Pipe(handler) {
-        var self = this;
-        if (!(this instanceof (Pipe))) {
-            self = new Pipe(handler);
-        } else {
-            self._previous = null;
-            self._next = null;
-            self._handler = handler;
-            self._root = {
-                status: IDLE,
-                root: self
+    function wrapperHandler(key, handler) {
+
+        var config = handler[key] ||
+            {
+                _next: null,
+                _previous:null
             };
+
+        handler[key] = config;
+
+    }
+
+    function getNext(key, handler) {
+        return handler[key] && handler[key]._next || null;
+    }
+    function getPrevious(key,handler){
+        return handler[key] && handler[key]._previous || null;
+    }
+
+    function setNext(key, handler, nextHandler) {
+        var config = handler[key];
+        if (!config) {
+            throw new Error('handler is not in the pipe');
+        }
+        if (nextHandler[key]) {
+            throw new Error('nextHandler is already in the pipe');
+        }
+        wrapperHandler(key, nextHandler);
+        var nextConfig = nextHandler[key];
+        nextConfig._next = config._next;
+        if(config._next){
+            config._next[key]._previous = nextHandler;
+        }
+        nextConfig._previous = handler;
+        config._next = nextHandler;
+        
+
+    }
+    function setPrevious(key, handler, previousHandler) {
+        var config = handler[key];
+        if (!config) {
+            throw new Error('handler is not in the pipe');
+        }
+        if (previousHandler[key]) {
+            throw new Error('previousHandler is already in the pipe');
+        }
+        wrapperHandler(key, previousHandler);
+        var previousConfig = previousHandler[key];
+        if(config._previous){
+            config._previous[key]._next = previousHandler;
+        }
+        previousConfig._previous = config._previous;
+        previousConfig._next = handler;
+        config._previous = previousHandler;
+    }
+  
+
+
+    function Pipe(first, last) {
+        var self = this;
+        first = first || firstNoop;
+        last = last || lastNoop;
+        if (!(self instanceof (Pipe))) {
+            self = new Pipe(first, last);
+        } else {
+            self._key = key();
+            wrapperHandler(self._key, first);
+            self._first = first;
+            self._last = last;
+            setNext(self._key,first,last);
         }
         return self;
     }
     Pipe.prototype = {
         constructor: Pipe,
         source: function (source) {
-            if (this._root.status === IDLE) {
-                next(this._root.root, source);
-            } else {
-                throw new Error();
-            }
-
+            next(this._key,this._first, source);
         },
-        remove: function () {
-            this._root = {
-                status: IDLE,
-                root: this
-            }
-            var next = this._next;
-            var previous = this._previous;
+        unshift:function(handler){
+            setNext(this._key,this._first,handler);
+        },
+        append: function (handler) {
+            setPrevious(this._key,this._last,handler)
+        },
+        remove: function (handler) {
+            var config = handler[this._key];
+            var next = getNext(this._key, handler);
+            var previous = getPrevious(this._key, handler);
             if (next && previous) {
-                next._previous = previous;
-                previous._next = next;
-            } else if (!next && previous) {
-                previous._next = null;
-            } else if (!previous && next) {
-                next._previous = null;
-                next._root.root = next;
+                previous[this._key]._next = next;
+                next[this._key]._previous = previous;
             }
 
         },
-        before: function (pipe) {
-            if (pipe._next) {
-                throw new Error();
-            }
-
-            pipe._next = this;
-            pipe._previous = this._previous;
-            this._previous = pipe;
-            pipe._root = this._root;
-            if (!pipe._previous) {
-                pipe._root.root = pipe;
-            }
-
+        insertBefore: function (handler, targetHandler) {
+            setPrevious(this._key,targetHandler,handler);
         },
-        after: function (pipe) {
-            if (pipe._previous) {
-                throw new Error();
-            }
-            pipe._previous = this;
-            pipe._next = this._next;
-            this._next = pipe;
-            pipe._root = this._root;
+        insertAfter: function (handler, targetHandler) {
+            setNext(this._key,targetHandler,handler);
         }
     }
     return Pipe;
